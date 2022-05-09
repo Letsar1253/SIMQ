23e25d8d4fdf781e0bddeeb2915e345f6ae4d460
PankovMA <pankovm2001@gmail.com>
2022-05-10 00:32:30 +0700

    Добавил новые процессы

---
+++ SimQCore/Library/Processes/GaussianProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SimQCore.Library.Distributions;
+
+namespace SimQCore.Processes
+{
+    public class GaussianProcess : IProcess
+    {
+        private double _q;
+        private double _sigma;
+        private NormalDistribution _normalDistribution;
+        private double _lastValue = 0;
+
+        public GaussianProcess(double q, double sigma)
+        {
+            _normalDistribution = new NormalDistribution(0, 1);
+            _q = q;
+            _sigma = sigma;
+        }
+
+        public double Generate()
+        {
+            _lastValue = _lastValue * Math.Exp(-1 * _q)
+                         + _sigma * _normalDistribution.Generate() * Math.Sqrt(1 - Math.Exp(-2 * _q));
+            return _lastValue;
+        }
+    }
+}
---
+++ SimQCore/Library/Processes/WienerProcess.cs
@@ -0,0 +1,25 @@
+﻿using SimQCore.Library.Distributions;
+
+namespace SimQCore.Processes
+{
+    public class WienerProcess : IProcess
+    {
+        private NormalDistribution _normalDistribution;
+        private double _lastValue = 0;
+        private double _alpha;
+        private double _sigma;
+
+        public WienerProcess(double alpha, double sigma)
+        {
+            _alpha = alpha;
+            _sigma = sigma;
+            _normalDistribution = new NormalDistribution(0, 1);
+        }
+
+        public double Generate()
+        {
+            _lastValue += _normalDistribution.Generate() * _sigma + _alpha;
+            return _lastValue;
+        }
+    }
+}
